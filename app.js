0a2724697bdc8a06c9768433c814012d86a3531f
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-09-19 14:06:06 +0530

    mongoose connect and add our first entry

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true, 
+    // useCreateIndex: true, 
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open',() =>{
+    console.log('dababase connected :)')
+})
+
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground',async (req, res)=>{
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'})
+    await camp.save()
+    res.send(camp)
 })
 
 app.listen(3000, () => {
---
+++ models/campground.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const campgroundSchema = new Schema({
+    title: String, 
+    price: String, 
+    description: String, 
+    location: String, 
+})
+
+module.exports = mongoose.model('Campground', campgroundSchema)

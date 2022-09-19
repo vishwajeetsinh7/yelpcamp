1534b1bd3c9583b67de3c39c4887ac674b3f16f4
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-09-19 13:38:17 +0530

    first day of yelpcamp

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000')
+})

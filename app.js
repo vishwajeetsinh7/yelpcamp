06f3ec360fb1baa131fcfe84c8afcfa9758bf6b6
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-09-21 17:13:32 +0530

    delete

--- app.js
+++ app.js
@@ -67,6 +67,14 @@ app.put('/campgrounds/:id/', async(req,res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id/', async(req,res) => {
+    const {id} = req.params
+    const campground =   await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+
+} )
+
+
 app.listen(3000, () => {
     console.log('App is running on port 3000')
 })

dd23ee99c62ae40c771b5cbfc6066f1ddecb5f95
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-11-02 11:51:32 +0530

    heroku nothing

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
-const Review = require('./models/review')
-const ejsMate = require('ejs-mate')
 
+const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
-const {campgroundSchema, reviewSchema} = require('./schemas')
-const catchAsync = require('./utils/catchAsync')
+
+const port = process.env.PORT || 3000 
+
+
+
+const campgrounds = require('./routes/campground')
+const reviews = require('./routes/reviews')
 
 
 const Joi = require('joi')
@@ -39,108 +42,37 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// without this parsing the body data is not workng
+app.use(express.urlencoded({extended:true}))
 
-const validateCampground = (req,res,next) =>{
+//  this is used for serving static file public directory
+app.use(express.static(path.join(__dirname, 'public')))
 
-    const {error} = campgroundSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next()
-    }
 
-}
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next()
-    }
-}
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground',async (req, res)=>{
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'})
-    await camp.save()
-    res.send(camp)
-})
-
-app.get('/campgrounds', async(req,res) => {
-    const campgrounds  = await Campground.find({})
-    res.render('campgrounds/index',{campgrounds})
-})
-
-app.get('/campgrounds/new', (req,res) => {
-    res.render('campgrounds/new')
-})
-// without this parsing the body data is not workng
-app.use(express.urlencoded({extended:true}))
 
 
-app.post('/campgrounds', validateCampground, catchAsync(async (req,res,next)=>{
-
-    if(!req.body.campground) throw new ExpressError('Invalid CampGround data', 400)
-    const campground = new Campground(req.body.campground)
-        await campground.save()
-        res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews')
-    console.log(campground)
-    res.render('campgrounds/show', {campground})
-}))
- 
-app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
-    const campground = await   Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground})
-}))
-
-app.put('/campgrounds/:id/',validateCampground, catchAsync(async(req,res) => {
-    const {id} = req.params
-    const campground = await    Campground.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/', catchAsync(async(req,res) => {
-    const {id} = req.params
-    const campground =   await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-
-}))
-
-// review route
-app.post('/campgrounds/:id/reviews', validateReview ,catchAsync(async (req,res) => {
-    const campground =await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req,res) => {
-    // res.send('delete me ')
-    const {id, reviewId}  = req.params
-    const campground =  await Campground.findByIdAndUpdate(id, {$pull : {reviews: reviewId}})
-    await  Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
 
 
-app.all('*', (req,res,next) => {
-    next(new ExpressError('Page not Found', 404))
+
+
+
+
+
+
+// app.all('*', (req,res,next) => {
+//     next(new ExpressError('Page not Found', 404))
     
-})
+// })
+
+
 app.use((err, req, res ,next) =>{
     const {statusCode = 500, message = 'Somethign not Riiight'} = err
     if(!err.message) err.message = 'Oh there is something Wrong'
@@ -148,6 +80,6 @@ app.use((err, req, res ,next) =>{
 })
 
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000')
+app.listen(port, () => {
+    console.log(`Yelpcamp running on http://localhost:${port}`)
 })

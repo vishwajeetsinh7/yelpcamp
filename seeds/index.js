d91595ee18efc2056671e29409ed7e090abbf996
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-12-13 17:46:15 +0530

    map add

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async() => {
     await Campground.deleteMany({})
-    for(let i=0; i<10; i++){
+    for(let i=0; i<200; i++){
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20 + 10)
         const camp = new Campground({
@@ -34,6 +34,13 @@ const seedDB = async() => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'this is very good place i like so much this place. good hotels and restorants and resorts',
             price: 10,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude, 
+                    cities[random1000].latitude
+                ]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
